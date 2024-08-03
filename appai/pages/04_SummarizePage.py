76adc609fe6d890b7c41d348b282c8f868c397e6
connor lee <connorlee184@gmail.com>
2024-08-03 11:47:50 -0700

    add page

--- appai/pages/04_SummarizePage.py
+++ appai/pages/04_SummarizePage.py
@@ -5,7 +5,7 @@ import streamlit as st
 from streamlit_chat import message
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from common.RAG import *
+#from common.RAG import *
 import tempfile
 
 def main():
