f7489603f2b5efc4a554e7fa6e6b67fde8026f05
connor lee <connorlee184@gmail.com>
2024-08-03 12:09:24 -0700

    update page

--- appai/pages/04_SummarizePage.py
+++ appai/pages/04_SummarizePage.py
@@ -5,13 +5,28 @@ import streamlit as st
 from streamlit_chat import message
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-#from common.RAG import *
+from common.summary import *
 import tempfile
+
+def filePath(file):
+    with tempfile.NamedTemporaryFile(delete=False) as tf:
+        if file is not None:
+            tf.write(file.getbuffer())
+            file_path = tf.name
+            return file_path
+
+def fileUpload():
+    uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")    
+    file_path = None
+    if filePath(uploaded_file) is not None:
+        file_path = filePath(uploaded_file)
+
+    return file_path
 
 def main():
     st.title("Page Summarizer")
-    st.write("Page")
-    st.write("Page")
+
+    fileUpload()
 
 if __name__ == "__main__":
     main()
