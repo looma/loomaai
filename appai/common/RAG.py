dd3d4bab24681e9bdf9063dcad3ca2f18babc7d0
connor lee <connorlee184@gmail.com>
2024-08-14 19:12:51 -0700

    comments for files

--- appai/common/RAG.py
+++ appai/common/RAG.py
@@ -11,29 +11,66 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.runnable import RunnablePassthrough
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from config import *
+from common.config import *
 from langchain_openai import ChatOpenAI
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 class gen: 
     def __init__(self):
+        """
+        Initializes model, tokenizer
+        - Gets API key from config.py 
+        - Initializes model 
+        - Creates prompt template 
+            - Question, Context where question is the user's query and context is the relevant documents for the RAG
+        - Creates document chunker 
+            - chunk_size: number of words in each document chunk
+            - chunk_overlap: number of words shared between each chunk in order to maintain context 
+        """
         cfg = ConfigInit()
         openai_api_key = cfg.getv("openai_api_key")
-        self.llm = ChatOpenAI(temperature=0, model_name="gpt-4o", api_key=openai_api_key)
+
+        self.llm = ChatOpenAI(temperature=0, model_name="gpt-4o", api_key=openai_api_key) # Intializes model
         #self.llm = ChatOllama(model="llama3")
-        self.prompt = PromptTemplate.from_template("Question: {question} \n Context: {context}")
-        self.textSplitter = RecursiveCharacterTextSplitter(chunk_size = 200, chunk_overlap = 20)
+
+        # Creates prompt template in the form of Question, Context where question is the user's query and context is the relevant documents for the RAG
+        self.prompt = PromptTemplate.from_template("Question: {question} \n Context: {context}") # Prompt template 
+
+        # Creates document chunker
+        self.textSplitter = RecursiveCharacterTextSplitter(chunk_size = 200, chunk_overlap = 20) # Splits text
 
     def makeChain(self, pdfPath):
-        pdf = PyMuPDFLoader(pdfPath).load()
-        chunks = self.textSplitter.split_documents(pdf)
+        """
+        Makes chain
+        - Creates pdf loader
+            - Takes in pdfPath which is path to file
+        - Chunks text
+            - Chunks the loaded pdf 
+        - Embedding function
+            - FastEmbed embeddings
+                - Fast, python lightweight embeddings
+            - HuggingFace embeddings
+                - Hugging face sentence-transformers/all-mpnet-base-v2
+        - Vector store
+            - Chroma
+                - Can be used standalone 
+            - FAISS
+                - Depends on vector_db which is generated from generate.py
+        - Retriever
+            - Searches vector store by similary-search and takes in top 3 documents
+        - Chain
+            - Chains together the context, which is retrieved from the retriever, query, llm
+        """
+        pdf = PyMuPDFLoader(pdfPath).load() # Loads pdf
+        chunks = self.textSplitter.split_documents(pdf) # Splits pdf
         
         # FastEmbed embeddings
-        #embedding_func = FastEmbedEmbeddings() #OllamaEmbeddings(model="llama3")
+        embedding_func = FastEmbedEmbeddings() #OllamaEmbeddings(model="llama3")
 
         # Hugging Face Embeddings
         
+        '''
         model_name = "sentence-transformers/all-mpnet-base-v2"
         model_kwargs = {}
         encode_kwargs = {'normalize_embeddings': False}
@@ -41,28 +78,26 @@ class gen:
             model_name=model_name,
             model_kwargs=model_kwargs,
             encode_kwargs=encode_kwargs)
+        '''
            
 
-
         # Chroma vector store
-        #vectorStore = Chroma.from_documents(chunks, embedding=embedding_func)
+        vectorStore = Chroma.from_documents(chunks, embedding=embedding_func)
 
         # Faiss vector store
+        '''
         path = "../../loomadata/vector_db"
-        #path = "/app/data/vector_db"
-        vectorStore = FAISS.load_local(path, embedding_func, allow_dangerous_deserialization=True)
+        path = "/app/data/vector_db"
+        #vectorStore = FAISS.load_local(path, embedding_func, allow_dangerous_deserialization=True)
+        '''
 
-        self.retriever = vectorStore.as_retriever(search_type = "similarity_score_threshold", search_kwargs={"k": 3, "score_threshold": 0.1,})
+        self.retriever = vectorStore.as_retriever(search_type = "similarity_score_threshold", search_kwargs={"k": 3, "score_threshold": 0.1,}) # Retriever for k = 3 top documents 
 
-        self.chain = {"context" : self.retriever , "question" : RunnablePassthrough()} | self.prompt | self.llm | StrOutputParser() 
+        self.chain = {"context" : self.retriever , "question" : RunnablePassthrough()} | self.prompt | self.llm | StrOutputParser()  # Initalizes chain
 
 
     def ask(self, query: str): 
-        return self.chain.invoke(query)
-
-
-#""" Test Code 
-test = gen()
-test.makeChain("/Users/connorlee/Documents/GitHub/loomaai/appai/textbooks/Class10/Math/textbook_chapters/10M01.pdf")
-print(test.ask("Can you give me a summary in 50 words"))
-#"""
+        """
+        Queries the chain and returns the output from the chain
+        """
+        return self.chain.invoke(query) # Returns output from chain with parameter user query 
--- appai/pages/03_Prompt.py
+++ appai/pages/03_Prompt.py
@@ -10,78 +10,98 @@ from common.RAG import *
 import tempfile
 
 def filePath(file):
-    with tempfile.NamedTemporaryFile(delete=False) as tf:
+    """
+    Takes in a file and creates a temporary file directiory (for streamlit)
+    - Returns a file path where the file is stored temporarily 
+    """
+    with tempfile.NamedTemporaryFile(delete=False) as tf: # tf is temporary file 
         if file is not None:
-            tf.write(file.getbuffer())
-            file_path = tf.name
-            return file_path
+            tf.write(file.getbuffer()) # Write the file information in tf 
+            file_path = tf.name # Assigns file path the temporary file path name 
+            return file_path # Returns file path
     
 def fileUpload(key):
-    uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf", key=key)    
+    """
+    Creates a file uploader in streamlit and returns path chosen by user
+    - key is a unique associated with the file_uploader within streamlit 
+    - checks if user uploaded a file or not, if so it returns the path by calling filePath(file)
+    """
+    uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf", key=key) # Creates file uploader
     file_path = None
-    if filePath(uploaded_file) is not None:
-        file_path = filePath(uploaded_file)
+    if filePath(uploaded_file) is not None: # Null check 
+        file_path = filePath(uploaded_file) # Gets temporary file path 
 
-    return file_path
+    return file_path 
 
 
 def page():
+    """
+    Initializes page
+    - Depends on choice(), common.RAG
+    - Uses file_path recieved from choice() to invoke chain with user query
+    """
 
     st.title("RAG implement")
 
-    file_path = choice()
+    file_path = choice() # Creates choice 
 
     query = st.text_input("User input")
 
     if file_path is not None:
-        model = gen()
-        model.makeChain(file_path)
+        model = gen() # Initializes model from RAG.py
+        model.makeChain(file_path) # Makes chain 
 
-        response = model.ask(query)
+        response = model.ask(query) # Gets response from chain 
 
         st.write(response)
     else:
         st.write("No current file")
 
 def textbooks(): 
+    """
+    Creates dropboxes with all the textbooks generated from split.py
+    """
     textbooks_path = os.path.join(os.path.join(os.path.dirname(__file__), '..'), 'textbooks')
-    files = os.listdir(textbooks_path)
+    files = os.listdir(textbooks_path) # Files of all textbooks 
 
-    classes = []
+    classes = [] # List for classes 
 
-    for file in files:
-        classes.append(file)
+    for file in files: 
+        classes.append(file) # Appends all the classes ex: Class1, Class2, etc 
 
-    classSelected = st.selectbox("Class", (classes))
+    classSelected = st.selectbox("Class", (classes)) # Creates dropbox for classes
 
-    class_path = os.path.join(os.path.join(os.path.dirname(__file__), '../textbooks/'), classSelected)
-    subjects = os.listdir(class_path)
+    class_path = os.path.join(os.path.join(os.path.dirname(__file__), '../textbooks/'), classSelected) # Enters directory for class selected
+    subjects = os.listdir(class_path) # Lists all files within dir
 
-    subjects_available = []
+    subjects_available = [] # List for all subjects avaiable within selected class
 
     for subject in subjects:
-        subjects_available.append(subject)
+        subjects_available.append(subject) # Appends all the subjects ex: Math, Nepali, etc. 
 
-    subjectSelected = st.selectbox("Subject", (subjects_available))
+    subjectSelected = st.selectbox("Subject", (subjects_available)) # Creates dropbox for subjects 
 
-    subject_books_path = os.path.join(os.path.dirname(__file__), '../textbooks/' + classSelected + "/" + subjectSelected + "/textbook_chapters")
-    subject_books = os.listdir(subject_books_path)
+    subject_books_path = os.path.join(os.path.dirname(__file__), '../textbooks/' + classSelected + "/" + subjectSelected + "/textbook_chapters") # Enters directory of subject selected
+    subject_books = os.listdir(subject_books_path) # Lists all files within dir
 
-    subject_books_available = []
+    subject_books_available = [] # List for all books avaiable within subject dir
 
     for book in subject_books:
-        subject_books_available.append(book)
+        subject_books_available.append(book) # Appends all possible books available 
 
-    book_selected = st.selectbox("Book", (subject_books_available))
+    book_selected = st.selectbox("Book", (subject_books_available)) # Creates dropbox for book selection
 
-    return os.path.join(os.path.dirname(__file__), '../textbooks/' + classSelected + "/" + subjectSelected + "/textbook_chapters/" + book_selected)
+    return os.path.join(os.path.dirname(__file__), '../textbooks/' + classSelected + "/" + subjectSelected + "/textbook_chapters/" + book_selected) # Final file path for selected book
 
 def choice():
+    """
+    Creates selection which allows user to either upload a file or choose from existing textbooks
+    """
     file_path = None 
-    choice = st.radio("Choose an option:", ("Upload a file", "Choose existing textbook"))
+    choice = st.radio("Choose an option:", ("Upload a file", "Choose existing textbook")) # Creates choice for user
 
     if choice == "Upload a file":
-        uploaded = fileUpload("03_1")
+        uploaded = fileUpload("03_1") 
         file_path = None if uploaded is None else uploaded
     else:
         file_path = textbooks()
@@ -89,8 +109,8 @@ def choice():
     return file_path
     
 def main():
-    page()
+    page() # Initlaizes page 
 
 
 if __name__ == "__main__":
-    main()
+    main() 
