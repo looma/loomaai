0ef02e2fb74a14317837eb4e70e70e3ea9f1b1b2
connor lee <connorlee184@gmail.com>
2024-08-14 19:12:51 -0700

    comments for files

--- appai/cli/RAG.py
+++ appai/cli/RAG.py
@@ -1,8 +1,21 @@
+"""
+For user CLI tests for RAG 
+"""
+
 import os
 import sys
+from langchain_openai import ChatOpenAI
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from common.RAG import *
+from common.config import *
 
-test = gen()
+def test_RAG(query:str):
+    test = gen()
+    test.makeChain("") # Path to file for RAG
+    out = test.ask(query)
+
+    return out
+
+test_RAG("Give me a summary in 50 words")
