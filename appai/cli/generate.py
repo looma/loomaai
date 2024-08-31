ee186be75555c5747c99dfc308e05438df3c3dbd
Atulya Weise <atultw@outlook.com>
2024-08-31 11:53:03 -0700

    switch to qdrant

--- appai/cli/generate.py
+++ appai/cli/generate.py
@@ -1,13 +1,30 @@
 import argparse
 
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
+from langchain_qdrant import QdrantVectorStore
 from pymongo import MongoClient
+from qdrant_client import QdrantClient
 
-from ..common.generate import generate_vectors
+from ..common.generate import generate_vectors, create_collection_if_not_exists
+
 parser = argparse.ArgumentParser(description="Embedding tool")
 parser.add_argument("apikey", type=str, help="The OpenAI API key")
 args = parser.parse_args()
 
 llm = ChatOpenAI(temperature=0, model_name="gpt-4o", api_key=args.apikey)
 client = MongoClient("mongodb://localhost:47017/")
-generate_vectors(llm=llm, mongo_client=client, data_dir="data")
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {}
+encode_kwargs = {'normalize_embeddings': False}
+hf = HuggingFaceEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+qclient = QdrantClient(url='http://localhost:46333')
+create_collection_if_not_exists("activities", qclient)
+qdrant = QdrantVectorStore.from_existing_collection(embedding=hf,
+                                                    collection_name="activities",
+                                                    url="http://localhost:46333")
+generate_vectors(llm=llm, mongo_client=client, vector_db=qdrant)
