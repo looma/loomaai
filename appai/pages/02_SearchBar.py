46c99d141af097cbb04179b99915ea9ff43cce48
Atulya Weise <atultw@outlook.com>
2024-09-04 13:25:16 -0700

    more qdrant updates

--- appai/pages/02_SearchBar.py
+++ appai/pages/02_SearchBar.py
@@ -5,7 +5,26 @@ from streamlit_pdf_viewer import pdf_viewer
 from common.query_faiss import query
 from common.config import *
 
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from pymongo import MongoClient
+from qdrant_client import QdrantClient
+
 st.title("Looma Content Search")
+
+client = MongoClient("mongodb://host.docker.internal:47017/")
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {}
+encode_kwargs = {'normalize_embeddings': False}
+hf = HuggingFaceEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+qclient = QdrantClient(url='http://qdrant:6333')
+qdrant = QdrantVectorStore.from_existing_collection(embedding=hf,
+                                                    collection_name="activities",
+                                                    url="http://qdrant:6333")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -18,7 +37,7 @@ if prompt := st.chat_input("Search Message ..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     cfg = ConfigInit()
     with st.chat_message("user"):
-        results = query(prompt, cfg.getv('datadir'))
+        results = query(prompt, qdrant)
         st.json([e.dict()["metadata"] for e in results])
         # for e in results:
-            # pdf_viewer(cfg.getv('datadir') + "/" + e.dict()["metadata"]["source"])
+        # pdf_viewer(cfg.getv('datadir') + "/" + e.dict()["metadata"]["source"])
