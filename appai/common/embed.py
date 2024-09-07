9df40100fcf6d200a8d824b2953367968e49a88b
Atulya Weise <atultw@outlook.com>
2024-09-07 11:08:37 -0700

    clean up

--- appai/common/embed.py
+++ appai/common/embed.py
@@ -31,39 +31,10 @@ def get_visible_text(url):
 
 def generate_vectors(llm, mongo_client: MongoClient, vector_db: QdrantClient):
     db = mongo_client.get_database("looma")
-    chapters_collection = db.get_collection("chapters")
     activities_collection = db.get_collection("activities")
-
-    chapters = chapters_collection.find({"pn": {"$ne": ""}})
-    print(f'{chapters_collection.count_documents({"pn": {"$ne": ""}})} chapters to be processed')
 
     activities = activities_collection.find({"ft": "html"})
     print(f'{activities_collection.count_documents({"ft": "html"})} activities to be processed')
-
-    # for i, chapter in enumerate(chapters):
-    #     try:
-    #         groups = re.search(r"([1-9]|10|11|12)(EN|ENa|Sa|S|SF|Ma|M|SSa|SS|N|H|V|CS)[0-9]{2}(\.[0-9]{2})?",
-    #                            chapter['_id'], re.IGNORECASE)
-    #         grade_level = groups[1]  # grade level
-    #         subject = groups[2]
-    #         firstPage = chapter['pn']
-    #         lastPage = chapter['pn'] + chapter['len']
-    #         if firstPage == "" or lastPage == "":
-    #             continue
-    #         textbook = db.textbooks.find_one({"prefix": grade_level + subject})
-    #         subpath = f"/files/chapters/{textbook['fp']}textbook_chapters/{chapter['_id']}.pdf"
-    #         url = f"{data_dir}{subpath}"
-    #         text = extract_text_from_pdf(url, "English")
-    #         summary = summarize_text(llm, text, "English")
-    #         final_docs = [Document(page_content=summary,
-    #                                metadata={"collection": "chapters", "source_id": textbook['_id']})]
-    #         if faiss_db is None:
-    #             faiss_db = FAISS.from_documents(final_docs, hf)
-    #         faiss_db.add_documents(final_docs)
-    #         faiss_db.save_local(f"{data_dir}/vector_db")
-    #         print(f"[{i}] [Added document to FAISS]", url, firstPage, lastPage)
-    #     except Exception as e:
-    #         print("Error: ", e, "Chapter:", chapter["_id"])
 
     for i, activity in enumerate(activities):
         try:
@@ -72,7 +43,6 @@ def generate_vectors(llm, mongo_client: MongoClient, vector_db: QdrantClient):
                     url = f"http://looma.website/{activity['fp']}{activity['fn']}"
                     print(url)
                     text = get_visible_text(url)
-                    # summary = summarize_text(llm, text, "English")
                     model_name = "sentence-transformers/all-mpnet-base-v2"
                     model_kwargs = {}
                     encode_kwargs = {'normalize_embeddings': False}
