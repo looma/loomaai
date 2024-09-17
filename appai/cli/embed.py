10b91841125c7596c1334966d110a161ea276ddf
Atulya Weise <atultw@outlook.com>
2024-09-16 18:44:32 -0700

    remove api key arg and document

--- appai/cli/embed.py
+++ appai/cli/embed.py
@@ -7,12 +7,12 @@ from pymongo import MongoClient
 from qdrant_client import QdrantClient
 
 from ..common.embed import generate_vectors, create_collection_if_not_exists
+#
+# parser = argparse.ArgumentParser(description="Embedding tool")
+# parser.add_argument("apikey", type=str, help="The OpenAI API key")
+# args = parser.parse_args()
 
-parser = argparse.ArgumentParser(description="Embedding tool")
-parser.add_argument("apikey", type=str, help="The OpenAI API key")
-args = parser.parse_args()
-
-llm = ChatOpenAI(temperature=0, model_name="gpt-4o", api_key=args.apikey)
+llm = ChatOpenAI(temperature=0, model_name="gpt-4o", api_key="")
 client = MongoClient("mongodb://localhost:47017/")
 
 qclient = QdrantClient(url='http://localhost:46333')
