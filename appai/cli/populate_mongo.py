976d28a19dd9dfe38b835d55d92549c09b797d22
Atulya Weise <atultw@outlook.com>
2024-10-05 14:42:41 -0700

    Add pre-population script and add pdf support to embed.py

---
+++ appai/cli/populate_mongo.py
@@ -0,0 +1,9 @@
+from ..common.embed import populate_relevant_resources
+
+from pymongo import MongoClient
+from qdrant_client import QdrantClient
+
+client = MongoClient("mongodb://localhost:47017/")
+
+qclient = QdrantClient(url='http://localhost:46333')
+populate_relevant_resources(mongo_client=client, vector_db=qclient)
