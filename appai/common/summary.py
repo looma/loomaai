e6acc26608ec9cf48f7967d5c5b14ee4e01f0880
alexgood111 <90157223+alexgood111@users.noreply.github.com>
2024-11-16 10:46:50 -0800

    06 and 07
    added quizzes and detail summary

--- appai/common/summary.py
+++ appai/common/summary.py
@@ -2,6 +2,9 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import fitz
+from langdetect import detect, LangDetectException
+import streamlit as st
+
 
 class Summary:
     def __init__(self, cfg, pdf_path):
@@ -20,33 +23,46 @@ class Summary:
         return text_content            
 
 #use the extracted text to send to OpenAI to summarize in ChatGPT
-    def summarize_text(self, text, chapter_language):
+    def summarize_text(self, text):
         summarize_prompt = PromptTemplate(
             input_variables=["text"],
-            template="{text}\nPlease summarize the following text in one paragraph in " + str(chapter_language)
+            template="Please summarize the following text in one paragraph in the same language it is written in \n if the text is in Nepali keep it in Nepali and if the text is in English keep it in English\n{text}"
         )
         summarize_chain = summarize_prompt | self.llm | StrOutputParser()
         summary = summarize_chain.invoke({"text": text})
         return summary
     
 #returning the summary
-    def summarize_pdf(self, chapter_language):
+    def summarize_pdf(self):
         text_content = self.extract_text()
-        summary = self.summarize_text(text_content, chapter_language)
+        summary = self.summarize_text(text_content)
         return summary
     
-#translating the text into either Nepali or English
-    def translate_text(self, text, tolanguage):
-        translate_prompt = PromptTemplate(
-                input_variables=["text"],
-                template="Please translate the following text:\n{text} to" + str(tolanguage)
+#returning the quiz
+    def quiz_pdf(self, text_content):
+        text_content = self.extract_text()
+        quiz = self.quiz_text(text_content)
+        return quiz
+    def quiz_text(self, text):
+        quiz_prompt = PromptTemplate(
+            input_variables=["text"],
+            template="Please make a quiz of the following text in the same language it is written in. Make an answer key but make sure answers are not shown at all on the quiz itself. If the text is in Nepali keep it in Nepali and if the text is in English keep it in English \n {text}"
         )
-        translate_chain = translate_prompt | self.llm | StrOutputParser()
-        translated_text = translate_chain.invoke({"text": text})
-        return translated_text
+        quiz_chain = quiz_prompt | self.llm | StrOutputParser()
+        quiz = quiz_chain.invoke({"text": text})
+        return quiz
     
-#Returning the translated text
-    def translate_pdf(self, language):
-        text_content = self.extract_text_from_pdf("Nepali")
-        translated_content = self.translate_text(text_content, language)
-        return translated_content
+    def detailSummary_text(self, text):
+        summarize_prompt = PromptTemplate(
+            input_variables=["text"],
+            template="Please summarize the following text in one paragraph in the same language it is written in \n if the text is in Nepali keep it in Nepali and if the text is in English keep it in English\n{text}"
+        )
+        summarize_chain = summarize_prompt | self.llm | StrOutputParser()
+        summary = summarize_chain.invoke({"text": text})
+        return summary
+    
+#returning the summary
+    def detailSummary_pdf(self):
+        text_content = self.extract_text()
+        summary = self.detailSummary_text(text_content)
+        return summary
