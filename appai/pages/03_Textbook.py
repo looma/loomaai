f227b3adb8f3a01e727a988b7b58c6374b2fc039
Praneel N <116457189+Gamelander4@users.noreply.github.com>
2024-11-23 10:49:53 -0800

    added tabs to textbook ui

--- appai/pages/03_Textbook.py
+++ appai/pages/03_Textbook.py
@@ -1,12 +1,35 @@
 import streamlit as st
+from pymongo import MongoClient
 
 from common.config import ConfigInit
 
 def TextbookUI(cfg):
     st.title('Textbook')
+    split, remove, dict, embed = st.tabs(['Split Textbook', 'Remove Textbook', 'Dictionary', 'Embed'])
+    
+    with split:
+        textbooks = st.text_input("What textbooks need to be split. Type 'all' for all the textbooks or the textbook prefix for a specific textbook")
 
+        if st.button("Split Chapters"):
+            with st.spinner("Spliting..."):
+                #gets the directory that the chapters have to go to  
+                cfg = ConfigInit()
+                datadir = cfg.getv("datadir")
+                
+                #calls the MongoClient and runs the split function in loomaai/appai/common/split.py
+                client = MongoClient("mongodb://host.docker.internal:47017/")
+                split(client, datadir+"/files/chapters", textbooks)
+                
+                st.write("all textbook chapters have their own pdfs")
+    
+    with remove:
+        st.write('hi')
+        
+    with dict:
+        st.write('hi')
 
-
+    with embed:
+        st.write('hi')
 
 
 if __name__ == '__main__':
