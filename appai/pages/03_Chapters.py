ecb15f194440cdebab516042eb69edb1853830fe
Atulya Weise <30200282+atultw@users.noreply.github.com>
2025-01-01 12:10:44 -0500

    add button to populate relevant activities

--- appai/pages/03_Chapters.py
+++ appai/pages/03_Chapters.py
@@ -1,13 +1,17 @@
 import os
-import re
 import tempfile
 import streamlit as st
 
 from common.activity_chapter import chapter_url_from_id
 from common.summary import *
+from common.embed import objectid_to_uuid
+from common.populate_relevant_resources import populate_resources_for_chapter
 from common.config import ConfigInit
 from common.streamlit_mongo_viewer import mongodb_viewer
 from pymongo import MongoClient
+from qdrant_client import QdrantClient
+
+
 # Defining the filepath for uploading the file that needs to be summarized
 def filePath(file):
     if file is not None:
@@ -17,24 +21,15 @@ def filePath(file):
             return file_path
     return None
 
-# Uploading the file into the filepath (allows multiple PDF uploads)
-def fileUpload(key):
-    uploaded_files = st.file_uploader('Choose your .pdf files', type="pdf", key=key, accept_multiple_files=True)
-    file_paths = []
-    
-    if uploaded_files:
-        for uploaded_file in uploaded_files:
-            file_path = filePath(uploaded_file)
-            if file_path is not None:
-                file_paths.append(file_path)
-
-    return file_paths
 
 def ChaptersUI(cfg):
     config = cfg.json()
     st.title("Chapters")
     MONGO_URI = f"mongodb://{config['mongo']['host']}:{config['mongo']['port']}"
     DATABASE_NAME = f"{config['mongo']['database']}"
+
+    qd = QdrantClient(url=f"http://{config['qdrant']['host']}:{config['qdrant']['port']}")
+
     selected_chapters = []
 
     with st.expander("Select Textbooks", expanded=True):
@@ -62,15 +57,21 @@ def ChaptersUI(cfg):
     client = MongoClient(MONGO_URI)
     db = client['looma']
 
-    # TODO: get the file names for the selected chapters
+    populate_tab, summary_tab, quiz_tab = st.tabs(
+        ["Populate Relevant Resources", "Summary", "Quiz"])  # TODO: add more prompt types
 
-    summary1, quiz1, translate1, topic1 = st.tabs(["Summary", "Quiz", "Translate", "Topic"])
-    with summary1:
+    with populate_tab:
+        if len(selected_chapters) > 0:
+            st.info("Make sure to embed all activities first (sidebar -> Activities)")
+            if st.button("Populate Relevant Activities", key="populate_button"):
+                with st.spinner("Populating..."):
+                    mongo_activies = list(db.activities.find({"ID": {"$in": [chapter["_id"] for chapter in selected_chapters]}}))
+                    qd_chapters = qd.retrieve(collection_name="activities", ids=[objectid_to_uuid(str(activity["_id"])) for activity in mongo_activies], with_vectors=True, with_payload=True)
+                    for qd_chapter in qd_chapters:
+                        populate_resources_for_chapter(qd, db, qd_chapter)
+                st.success("Populated relevant activities in MongoDB for selected chapters")
 
-        # For the summary loading visualization
-        if "summary_generated" not in st.session_state:
-            st.session_state["summary_generated"] = False
-
+    with summary_tab:
         if len(selected_chapters) > 0:
             if st.button("Summarize Selection", key="summarize_button"):
                 summaries = []
@@ -91,22 +92,11 @@ def ChaptersUI(cfg):
 
                         summaries.append(summary)
 
-                    # Display summaries for each PDF
-                    for i, summary in enumerate(summaries):
-                        st.write(f"### Summary of File {i+1}")
-                        st.info(summary)
-
-
-                # Store the summaries in session state
-                st.session_state["content"] = summaries
-                st.session_state["summary_generated"] = True
-
-    with quiz1:
-
-        # For the quiz loading visualization
-        if "quizzes_generated" not in st.session_state:
-            st.session_state["quizzes_generated"] = False
-
+                # Display summaries for each PDF
+                for i, summary in enumerate(summaries):
+                    st.write(f"### Summary of File {i + 1}")
+                    st.info(summary)
+    with quiz_tab:
         if len(selected_chapters) > 0:
             if st.button("Generate Quizzes for Selection"):
                 quizzes = []
@@ -114,7 +104,7 @@ def ChaptersUI(cfg):
                     for chapter in selected_chapters:
                         file_path, textbook = chapter_url_from_id(chapter["_id"], textbook=None, mongo=db)
                         quizzer = Summary(cfg, file_path)
-                        
+
                         # Generate the summary
                         quiz = quizzer.quiz_pdf()
                         files_dir = config["datadir"]
@@ -126,95 +116,11 @@ def ChaptersUI(cfg):
                         with open(save_info, "w") as file:
                             file.write(quiz)
                         quizzes.append(quiz)
-                
                 # Display summaries for each PDF
                 for i, quiz in enumerate(quizzes):
-                    st.write(f"### Quiz of File {i+1}")
+                    st.write(f"### Quiz of File {i + 1}")
                     st.info(quiz)
 
-                # Store the summaries in session state
-                st.session_state["content"] = quizzes
-                st.session_state["quizzes_generated"] = True
-    # with supersummary1:
-    #     file_paths = fileUpload("04_2")
-    #
-    #     # For the summary loading visualization
-    #     if "summary_generated" not in st.session_state:
-    #         st.session_state["summary_generated"] = False
-    #
-    #     if file_paths:
-    #         if st.button("Summarize All PDFs", key="supersummaries_button"):
-    #             summaries = []
-    #             with st.spinner("Summarizing..."):
-    #                 for file_path in file_paths:
-    #                     summarizer = Summary(cfg, file_path)
-    #
-    #                     # Generate the summary
-    #                     summary = summarizer.detailSummary_pdf()
-    #                     summaries.append(summary)
-    #
-    #             # Display summaries for each PDF
-    #             for i, summary in enumerate(summaries):
-    #                 st.write(f"### Summary of File {i+1}")
-    #                 st.info(summary)
-    #
-    #             # Store the summaries in session state
-    #             st.session_state["content"] = summaries
-    #             st.session_state["summary_generated"] = True
-                
-    with translate1:
-        file_paths = fileUpload("04_3")
-
-        # For the quiz loading visualization
-        if "quizzes_generated" not in st.session_state:
-            st.session_state["quizzes_generated"] = False
-
-        if file_paths:
-            if st.button("Translations for all PDFs", key="quizzes_button"):
-                translations = []
-                with st.spinner("Generating..."):
-                    for file_path in file_paths:
-                        translater = Summary(cfg, file_path)
-                        
-                        # Generate the summary
-                        translate = translater.translate_pdf()
-                        translations.append(translate)
-                
-                # Display summaries for each PDF
-                for i, translate in enumerate(translations):
-                    st.write(f"### Translation of File {i+1}")
-                    st.info(translate)
-
-                # Store the summaries in session state
-                st.session_state["content"] = translations
-                st.session_state["quizzes_generated"] = True
-    with topic1:
-        file_paths = fileUpload("04_4")
-
-        # For the summary loading visualization
-        if "summary_generated" not in st.session_state:
-            st.session_state["summary_generated"] = False
-
-        if file_paths:
-            if st.button("Chapter Topics", key="Chapter_button"):
-                topics = []
-                with st.spinner("Analysing..."):
-                    for file_path in file_paths:
-                        topicizer = Summary(cfg, file_path)
-                        
-                        # Generate the summary
-                        topic = topicizer.topic_pdf()
-                        topics.append(topic)
-                    # Display summaries for each PDF
-                    for i, topic in enumerate(topics):
-                        st.write(f"### Topics of File {i+1}")
-                        st.info(topic)
-                
-
-                # Store the summaries in session state
-                st.session_state["content"] = topics
-                st.session_state["summary_generated"] = True
-                
 
 if __name__ == '__main__':
     try:
