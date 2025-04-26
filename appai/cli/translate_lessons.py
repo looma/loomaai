1d167c2956b9c04d9505264dd1bad156a71de60f
aweise <30200282+atultw@users.noreply.github.com>
2025-04-25 19:51:36 -0700

    make translate-missing-lessons

--- appai/cli/translate_lessons.py
+++ appai/cli/translate_lessons.py
@@ -4,10 +4,17 @@ from ..common.translate_lessons import translate_lessons
 
 from pymongo import MongoClient
 from langchain_openai import ChatOpenAI
+import argparse
 
 MONGO_URI = os.getenv("MONGO_URI")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 client = MongoClient(MONGO_URI)
 llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
 
-translate_lessons(db=client.get_database('looma'), llm=llm)
+
+parser = argparse.ArgumentParser(description="Translate lessons in the database.")
+parser.add_argument("--missing-only", action="store_true", help="Translate only missing lessons.")
+args = parser.parse_args()
+
+missing_only = args.missing_only
+translate_lessons(db=client.get_database('looma'), llm=llm, missing_only=missing_only)
