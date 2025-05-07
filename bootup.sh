df61c85cdc4c4afc23435fa976e9dc90172cd6e4
aweise <30200282+atultw@users.noreply.github.com>
2025-05-06 17:30:57 -0700

    remove old lines

--- bootup.sh
+++ bootup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-echo "Starting "
-
-# Here we setup the data directories, ollama etc 
-# needed for the app
-if [ -f /app/setup.sh ]; then
-    /app/setup.sh
-fi
-
-# Load the environment variables
-source /app/appai/.env 
-
 # Start the streamlit app
 cd /app/appai
 streamlit run main.py --server.port 4700 \
